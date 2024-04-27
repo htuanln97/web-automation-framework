5c2a6623c2c3b2ed591bb58c5a53d3750ab209ae
Tuan Le <hoangtuan.ln97@gmail.com>
2024-04-27 19:44:47 +0700

    add code for TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,13 +2,28 @@ package tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import com.microsoft.playwright.Page;
+
+import core.PlaywrightFactory;
 
 public class TestBase {
+	protected Page page;
+	PlaywrightFactory playwrightFactory;
+	
+	@Parameters({"broswerType"})
 	@BeforeMethod
-	public void beforeMethod() {
+	public void beforeMethod(@Optional("chrome") String broswerType) {
+		playwrightFactory = new PlaywrightFactory();
+		page = playwrightFactory.getPage(broswerType);
+		page.setDefaultTimeout(60000);
 	}
 
 	@AfterMethod
 	public void afterMethod() {
+		page.context().browser().close();
+		playwrightFactory.closePlaywright();
 	}
 }
